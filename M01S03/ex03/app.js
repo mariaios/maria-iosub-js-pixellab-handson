a58cc9d0b79af13bd9f1150fd20f5b91fd4ef5f5
Maria Iosub <maria.salajan8@gmail.com>
2021-06-15 21:46:49 +0300

    curs 15.06.21

---
+++ M01S03/ex03/app.js
@@ -0,0 +1,29 @@
+var person = {
+  name: 'Maria',
+  surname: 'Iosub',
+  friends: {},
+};
+
+var larry = {
+  name: 'Larry',
+  surname: 'Larryson',
+  friends: {},
+};
+
+var carol = {
+  name: 'Carol',
+  surname: 'Carolson',
+  friends: {},
+};
+
+var steven = {
+  name: 'Steven',
+  surname: 'Stevenson',
+  friends: {},
+};
+
+var andra = {
+  name: 'Andra',
+  surname: 'Andrason',
+  friends: {},
+};
